49dbb118c592aff30a6b55346cf1c31b7376a702
Shyamprasad <shyamprasad2000.sp@gamil.com>
2021-09-15 19:52:16 +0530

    shop

---
+++ shop/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import get_object_or_404, render
+from .models import *
+from django.db.models import Q
+from django.core.paginator import Paginator,EmptyPage,InvalidPage
+# Create your views here.
+def home(request,c_slug=None):
+    c_page=None
+    prodt=None
+    if c_slug!=None:
+        c_page=get_object_or_404(category,slug=c_slug)
+        prodt=product.objects.filter(categ=c_page,available=True)
+
+    else:
+        prodt=product.objects.all().filter(available=True)
+        
+    ct=category.objects.all()
+    paginator=Paginator(prodt,3)
+    try:
+        page=int(request.GET.get('page','1'))
+    except:
+        page=1
+
+    try:
+        pro=paginator.page(page)
+    except(EmptyPage,InvalidPage):
+        pro=paginator.page(paginator.num_pages)
+   
+    return render(request,'index.html',{'pr':prodt,'ct':ct,'pg':pro})
+
+
+
+def prodDetails(request,c_slug,product_slug):
+    try:
+        prod=product.objects.get(categ_slug=c_slug,slug=product_slug)
+    except Exception as e:
+        raise e
+    return render(request,'item.html',{'pr':prod})
+
+
+
+def searching(request):
+    prod=None
+    query=None
+    if 'q' in request.GET:
+        query=request.GET.get('q')
+        prod=product.objects.all().filter(Q(name__contains=query)|Q(desc__contains=query))
+
+    return render(request,'search.html',{'qr':query,'pr':prod})
